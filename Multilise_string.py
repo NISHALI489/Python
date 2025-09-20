4c1ea95e97e6ff4e3dee0636c9bec22e406fe6d7
ARDIANET\nmahaveervanjale <nishali.mahaveervanjale@expleogroup.com>
2025-09-20 20:20:16 +0530

    Added multiline comment and prited

---
+++ Multilise_string.py
@@ -0,0 +1,10 @@
+#Multiline string print
+
+Todays_special = """
+                 1. Maharashtrian thali included with mung daal varan with ghi yellow rice (Details)
+                 2. Chapati with shrikhand with added papad, aloo ki sabji
+                 3. Thali pith with curd and salad
+                 4. Highly Fiber added Poha with raw cocunt
+                """
+print("Todays special menu %s" %(Todays_special))
+print("For detail information click below link") 
