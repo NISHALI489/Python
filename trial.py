69602c1ba8db4767210c75162907192aa18cd561
ARDIANET\nmahaveervanjale <nishali.mahaveervanjale@expleogroup.com>
2025-09-20 14:05:13 +0530

    added py files

--- trial.py
+++ trial.py
@@ -1,1 +1,19 @@
-print("Hey All!")
+"""Write a pythin code for given simple req
+store the curise speed of car and
+store speed of vehicle as well
+then to showing error of vehicle speed it shall be difference between vehicle speed and curise speeduirements
+in variable and print the vehicle speed using print statement
+"""
+
+
+#storing a cruise speed of varible storing
+curise_speed = 40
+vehicle_speed = 120
+
+print ("The curise speed of car is = %d" %curise_speed)
+print ("The vehicle speed is = %d" %vehicle_speed)
+
+#showing error of vehicle speed
+error_speed = vehicle_speed - curise_speed
+
+print ("The error speed of car is = %d"%error_speed)
