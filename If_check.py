41c60ee90b682b23f1f50f83e2918c490fc8f708
ARDIANET\nmahaveervanjale <nishali.mahaveervanjale@expleogroup.com>
2025-09-24 18:59:32 +0530

    Added readme

---
+++ If_check.py
@@ -0,0 +1,13 @@
+#3 number greater
+
+a = 10
+b = 20
+c = 5
+
+if a > b and a > c:
+    print("a is greater : %d" %a)
+
+elif b > a and b > c:
+    print("b is greater %d" %b)
+else:
+    print("c is greater: %d" %c)
